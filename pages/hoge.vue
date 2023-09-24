229d40706a68da730cd0edfc4ef4238b1922b326
Sangun-Kang <sangun950@gmail.com>
2023-09-24 23:46:51 +0900

    ルーティング学びました

---
+++ pages/hoge.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const hello = 'hello world!'
+</script>
+<template>
+  <div>
+    This is hoge Page {{ hello }}
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const hello = 'hello world!'
+</script>
+<template>
+  <div>
+    This is main Page {{ hello }}
+  </div>
+</template>
